bcd9d01bd1e00803abc9b5da920483a7423863ae
waitforever <664473032@qq.com>
2019-01-18 00:32:43 +0800

    http message and handler lift

--- src/main/java/com/godme/example/first/MyHandler.java
+++ src/main/java/com/godme/example/first/MyHandler.java
@@ -7,16 +7,78 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 public class MyHandler  extends SimpleChannelInboundHandler<HttpObject> {
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
-        if(msg instanceof HttpObject){
-            ByteBuf content = Unpooled.copiedBuffer("hello , world", CharsetUtil.UTF_8);
-            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,HttpResponseStatus.OK,content);
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) {
+        System.out.println("channelRead0");
+        if(msg instanceof HttpRequest){
+            HttpRequest request = (HttpRequest)msg;
+            HttpMethod method = request.method();
+            URI uri = URI.create(request.uri());
+            String path = uri.getPath();
+            String contentString = "method\t: " + method.toString() + "\npath\t:" + path;
+            ByteBuf contentBuf = Unpooled.copiedBuffer(contentString, CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,HttpResponseStatus.OK,contentBuf);
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
-            response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH,contentBuf.readableBytes());
             ctx.writeAndFlush(response);
-
+            ctx.channel().close();
         }
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelRegistered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelActive");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelReadComplete");
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        System.out.println("userEventTriggered");
+        super.userEventTriggered(ctx, evt);
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelWritabilityChanged");
+        super.channelWritabilityChanged(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("exceptionCaught");
+        super.exceptionCaught(ctx, cause);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelUnregistered");
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelInactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("channelRead");
+        super.channelRead(ctx, msg);
     }
 }
